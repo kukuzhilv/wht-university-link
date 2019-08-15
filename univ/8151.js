ca4c9b2380b5174ea2b87ffab5f8d508ab322dc4
kukuzhilv <51789515+kukuzhilv@users.noreply.github.com>
2019-08-15 18:56:51 +0800

    Create 8151.js

---
+++ univ/8151.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "中国矿业大学",                        
+    id: " 8151",                              
+    link: [ {
+        name: "矿大主页",                     
+        url: "https://www.cumt.edu.cn/"   
+    }, {
+        name: "教务处",
+        url: "http://jwb.cumt.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.cumt.edu.cn/"
+    }, {
+        name: "教务管理平台",
+        url: "http://jwxt.cumt.edu.cn/jwglxt/xtgl/login_slogin.html?language=zh_CN&_t=1565866332360"
+    }, {
+        name: "环测主页",
+        url: "http://cesi.cumt.edu.cn/"
+    }]
+};
